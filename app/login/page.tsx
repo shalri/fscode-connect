b80bd70bea6a600b11753dc1f654f678520f3457
shalri <fsaonlinework777@gmail.com>
2025-01-19 00:19:51 +0800

    Scaffolding

---
+++ app/login/page.tsx
@@ -0,0 +1,14 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Login | FScode Connect",
+};
+
+export default function LoginPage() {
+  return (
+    <div>
+      <h1>Login component</h1>
+      {/* TODO: Login component  */}
+    </div>
+  );
+}
